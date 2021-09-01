e622830dab47a33e60f93eadeb63039682b9459e
Christoph Praschl <christoph.praschl@fh-hagenberg.at>
2021-09-01 08:09:26 +0200

    Exporting main results to csv and changed print to logging

--- src/geofiles/main.py
+++ src/geofiles/main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from os import listdir
 from os.path import isfile, join
@@ -20,8 +21,8 @@ if __name__ == '__main__':
     Size comparison using multiple obj base models.
     You can find examples e.g. here: https://github.com/alecjacobson/common-3d-test-models
     """
-    path = r"C:\Users\P41743\Desktop\samples"  # input("Path to sample .obj files: ")
-    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    path = input("Path to sample .obj files: ")
+    inputfiles = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith(".obj")]
     crs = "urn:ogc:def:crs:OGC:2:84"
     origin = [14.2842798233032, 48.30284881591775, 279.807006835938]
     reader = GeoObjReader()
@@ -49,46 +50,59 @@ if __name__ == '__main__':
     encoding["gml"] = True
     encoding["kml"] = True
 
+    number_of_jobs = 0
+    for value in writers.values():
+        number_of_jobs += 1
+        if value.supports_origin_base():
+            number_of_jobs += 1
+    number_of_jobs *= len(inputfiles)
+
     sizes = dict()
     origin_based_sizes = dict()
-    for file in onlyfiles:
-        print(file)
+    done_jobs = 0
+    for file in inputfiles:
         sizes_for_file = dict()
         origin_based_sizes_for_file = dict()
         geoobj = reader.read(join(path, file))
         converted = converter.from_local(geoobj, crs, origin, False)
         converted_origin_based = converter.from_local(geoobj, crs, origin, True)
         for key, value in writers.items():
-            print(f"---- {type(value)}")
             value: BaseWriter = value
             folder = join(path, key)
             os.makedirs(folder, exist_ok=True)
             target = join(folder, file)
             value.write(target, converted, encoding[key])
+            done_jobs += 1
+            logging.info(f"Step {done_jobs} of {number_of_jobs} done ({file}, {type(value).__name__})")
             sizes_for_file[key] = os.path.getsize(target+value.get_file_type())
             if value.supports_origin_base():
                 origin_target = join(folder, "origin_" + file)
                 value.write(origin_target, converted_origin_based, encoding[key])
+                done_jobs += 1
+                logging.info(f"Step {done_jobs} of {number_of_jobs} done ({file}, {type(value).__name__}, origin-based)")
                 origin_based_sizes_for_file[key] = os.path.getsize(origin_target + value.get_file_type())
 
         sizes[file] = sizes_for_file
         origin_based_sizes[file] = origin_based_sizes_for_file
 
-    print("-----------------------------------------------------------------")
+    with open(join(path, "result.csv"), "w") as file:
+        file.write("file")
+        for extension, writer in writers.items():
+            file.write(f",{extension}")
+            if writer.supports_origin_base():
+                file.write(f",{extension}-origin")
+        file.write("\n")
 
-    formats = ["cityjson","geojson","geoobj","geooff","geoply","geostl","geovrml","gml","kml"]
-    num_of_formats = len(formats)
-    print("File,cityjson,geojson,geoobj,geooff,geoply,geostl,geovrml,gml,kml")
-    for key, size in sizes.items():
-        origin_size = origin_based_sizes[key]
-        s = f"{key},"
-        for idx, file_format in enumerate(formats):
-            s += "{:.2f}".format(size[file_format] / 1024.0)
-            if origin_size.get(file_format) is not None:
-                s += " | "
-                s += "{:.2f}".format(origin_size[file_format] / 1024.0)
-            if idx < num_of_formats - 1:
-                s += ","
-        print(s)
+        for key, size in sizes.items():
+            origin_size = origin_based_sizes[key]
+            file.write(key)
+            for file_format in writers.keys():
+                file.write(",")
+                file.write("{:.2f}".format(size[file_format] / 1024.0))
+                if origin_size.get(file_format) is not None:
+                    file.write(",")
+                    file.write("{:.2f}".format(size[file_format] / 1024.0))
+            file.write("\n")
+
 
 
