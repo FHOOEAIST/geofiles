5aa08faea743cc4dbee47be8c215f3b7edb597e9
Christoph Praschl <christoph.praschl@fh-hagenberg.at>
2021-10-18 15:06:03 +0200

    #6 Adding CityJSON support

---
+++ tests/geofiles/reader/test_city_json_reader.py
@@ -0,0 +1,132 @@
+import json
+
+from geofiles.reader.city_json_reader import CityJsonReader
+from tests.geofiles.base_test import BaseTest
+
+
+class TestCityJsonReader(BaseTest):
+    def test_read(self) -> None:
+        # given
+        file = self.get_ressource_file("cube.city.json")
+        reader = CityJsonReader()
+        cube = self.get_cube()
+
+        # when
+        geo_obj_file = reader.read(file)
+
+        # then
+        self.assertTrue(geo_obj_file.is_geo_referenced())
+        self.assertFalse(geo_obj_file.is_origin_based())
+        self.assertEqual(len(geo_obj_file.objects), 1)
+        self.assertEqual(geo_obj_file.objects[0].name, "cube")
+        self.assertEqual(len(geo_obj_file.objects[0].faces), 12)
+        self.assertEqual(len(geo_obj_file.vertices), 8)
+        self.assertEqual(geo_obj_file.crs, "urn:ogc:def:crs:OGC:2:84")
+
+        for idx, face in enumerate(geo_obj_file.objects[0].faces):
+            for inneridx, i in enumerate(face.indices):
+                self.assertEqual(
+                    int(i), int(cube.objects[0].faces[idx].indices[inneridx])
+                )
+
+        for idx, vertex in enumerate(geo_obj_file.vertices):
+            self.assertAlmostEqual(float(vertex[0]), cube.vertices[idx][0])
+            self.assertAlmostEqual(float(vertex[1]), cube.vertices[idx][1])
+            self.assertAlmostEqual(float(vertex[2]), cube.vertices[idx][2])
+
+    def test_read_string(self) -> None:
+        # given
+        file = self.get_ressource_file("cube.city.json")
+        with open(file) as json_file:
+            j = json.load(json_file)
+            reader = CityJsonReader()
+            cube = self.get_cube()
+
+            # when
+            geo_obj_file = reader.read_string(json.dumps(j))
+
+            # then
+            self.assertTrue(geo_obj_file.is_geo_referenced())
+            self.assertFalse(geo_obj_file.is_origin_based())
+            self.assertEqual(len(geo_obj_file.objects), 1)
+            self.assertEqual(geo_obj_file.objects[0].name, "cube")
+            self.assertEqual(len(geo_obj_file.objects[0].faces), 12)
+            self.assertEqual(len(geo_obj_file.vertices), 8)
+            self.assertEqual(geo_obj_file.crs, "urn:ogc:def:crs:OGC:2:84")
+
+            for idx, face in enumerate(geo_obj_file.objects[0].faces):
+                for inneridx, i in enumerate(face.indices):
+                    self.assertEqual(
+                        int(i), int(cube.objects[0].faces[idx].indices[inneridx])
+                    )
+
+            for idx, vertex in enumerate(geo_obj_file.vertices):
+                self.assertAlmostEqual(float(vertex[0]), cube.vertices[idx][0])
+                self.assertAlmostEqual(float(vertex[1]), cube.vertices[idx][1])
+                self.assertAlmostEqual(float(vertex[2]), cube.vertices[idx][2])
+
+    def test_read_string2(self) -> None:
+        # given
+        file = self.get_ressource_file("cube.city.json")
+        with open(file) as json_file:
+            j = json.load(json_file)
+            del j["metadata"]
+            reader = CityJsonReader()
+            cube = self.get_cube()
+
+            # when
+            with self.assertRaises(Exception) as context:
+                geo_obj_file = reader.read_string(json.dumps(j))
+
+            # then
+            self.assertTrue("No metadata defined (at least reference system is required)" in str(context.exception))
+
+    def test_read_string3(self) -> None:
+        # given
+        file = self.get_ressource_file("cube.city.json")
+        with open(file) as json_file:
+            j = json.load(json_file)
+            metadata = j["metadata"]
+            metadata["yo"] = "test"
+            del metadata["referenceSystem"]
+            reader = CityJsonReader()
+            cube = self.get_cube()
+
+            # when
+            with self.assertRaises(Exception) as context:
+                geo_obj_file = reader.read_string(json.dumps(j))
+
+            # then
+            self.assertTrue("Unknown reference system in input file." in str(context.exception))
+
+    def test_read_string4(self) -> None:
+        # given
+        file = self.get_ressource_file("cube.city.json")
+        with open(file) as json_file:
+            j = json.load(json_file)
+            del j["vertices"]
+            reader = CityJsonReader()
+            cube = self.get_cube()
+
+            # when
+            with self.assertRaises(Exception) as context:
+                geo_obj_file = reader.read_string(json.dumps(j))
+
+            # then
+            self.assertTrue("Undefined vertices in input file." in str(context.exception))
+
+    def test_read_string5(self) -> None:
+        # given
+        file = self.get_ressource_file("cube.city.json")
+        with open(file) as json_file:
+            j = json.load(json_file)
+            del j["CityObjects"]
+            reader = CityJsonReader()
+            cube = self.get_cube()
+
+            # when
+            with self.assertRaises(Exception) as context:
+                geo_obj_file = reader.read_string(json.dumps(j))
+
+            # then
+            self.assertTrue("No city objects defined" in str(context.exception))
