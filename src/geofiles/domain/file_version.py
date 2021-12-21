25f95b564a4caf538e81ebb4ab102a78fc96229c
Christoph Praschl <christoph.praschl@fh-hagenberg.at>
2021-12-21 11:54:07 +0100

    #32 Adding CityJson 1.1 support to the reader

---
+++ src/geofiles/domain/file_version.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class CityJsonVersion(Enum):
+    """
+    Enum defining the CityJSON version
+    """
+
+    V1_0 = 0  # V 1.0
+    V1_1 = 1  # V 1.1
--- tests/geofiles/reader/test_city_json_reader.py
+++ tests/geofiles/reader/test_city_json_reader.py
@@ -1,20 +1,19 @@
 import json
 
+from geofiles.domain.file_version import CityJsonVersion
+from geofiles.domain.geo_object_file import GeoObjectFile
 from geofiles.reader.city_json_reader import CityJsonReader
 from tests.geofiles.base_test import BaseTest
 
 
 class TestCityJsonReader(BaseTest):
-    def test_read(self) -> None:
-        # given
-        file = self.get_ressource_file("cube1-0.city.json")
-        reader = CityJsonReader()
+    def _compare_cube(self, geo_obj_file: GeoObjectFile) -> None:
+        """
+        Compares the given geo obj file with the standard cube
+        :param geo_obj_file: to be compared
+        :return: None
+        """
         cube = self.get_cube()
-
-        # when
-        geo_obj_file = reader.read(file)
-
-        # then
         self.assertTrue(geo_obj_file.is_geo_referenced())
         self.assertFalse(geo_obj_file.is_origin_based())
         self.assertEqual(len(geo_obj_file.objects), 1)
@@ -34,12 +33,62 @@ class TestCityJsonReader(BaseTest):
             self.assertAlmostEqual(float(vertex[1]), cube.vertices[idx][1])
             self.assertAlmostEqual(float(vertex[2]), cube.vertices[idx][2])
 
+    def test_read(self) -> None:
+        # given
+        file = self.get_ressource_file("cube1-0.city.json")
+        reader = CityJsonReader(CityJsonVersion.V1_0)
+
+        # when
+        geo_obj_file = reader.read(file)
+
+        # then
+        self._compare_cube(geo_obj_file)
+
+    def test_read2(self) -> None:
+        # given
+        file = self.get_ressource_file("cube1-1.city.json")
+        reader = CityJsonReader(CityJsonVersion.V1_1)
+
+        # when
+        geo_obj_file = reader.read(file)
+
+        # then
+        self.assertEqual(geo_obj_file.scaling, [1, 2, 3])
+        self.assertEqual(geo_obj_file.translation, [4, 5, 6])
+        self._compare_cube(geo_obj_file)
+
+    def test_read3(self) -> None:
+        # given
+        file = self.get_ressource_file("cube1-0.city.json")
+        reader = CityJsonReader(CityJsonVersion.V1_1, True)
+
+        # when
+        geo_obj_file = reader.read(file)
+
+        # then
+        self._compare_cube(geo_obj_file)
+
+    def test_read4(self) -> None:
+        # given
+        file = self.get_ressource_file("cube1-0.city.json")
+        reader = CityJsonReader(CityJsonVersion.V1_1)
+
+        # when
+        with self.assertRaises(Exception) as context:
+            reader.read(file)
+
+        # then
+        self.assertTrue(
+            "Transform information is mandatory in CityJSON >= 1.1"
+            in str(context.exception)
+        )
+
     def test_read_string(self) -> None:
         # given
         file = self.get_ressource_file("cube1-0.city.json")
         with open(file) as json_file:
             j = json.load(json_file)
-            reader = CityJsonReader()
+            reader = CityJsonReader(CityJsonVersion.V1_0)
             cube = self.get_cube()
 
             # when
@@ -71,7 +120,7 @@ class TestCityJsonReader(BaseTest):
         with open(file) as json_file:
             j = json.load(json_file)
             del j["metadata"]
-            reader = CityJsonReader()
+            reader = CityJsonReader(CityJsonVersion.V1_0)
 
             # when
             with self.assertRaises(Exception) as context:
@@ -91,7 +140,7 @@ class TestCityJsonReader(BaseTest):
             metadata = j["metadata"]
             metadata["yo"] = "test"
             del metadata["referenceSystem"]
-            reader = CityJsonReader()
+            reader = CityJsonReader(CityJsonVersion.V1_0)
 
             # when
             with self.assertRaises(Exception) as context:
@@ -108,7 +157,7 @@ class TestCityJsonReader(BaseTest):
         with open(file) as json_file:
             j = json.load(json_file)
             del j["vertices"]
-            reader = CityJsonReader()
+            reader = CityJsonReader(CityJsonVersion.V1_0)
 
             # when
             with self.assertRaises(Exception) as context:
@@ -125,7 +174,7 @@ class TestCityJsonReader(BaseTest):
         with open(file) as json_file:
             j = json.load(json_file)
             del j["CityObjects"]
-            reader = CityJsonReader()
+            reader = CityJsonReader(CityJsonVersion.V1_0)
 
             # when
             with self.assertRaises(Exception) as context:
